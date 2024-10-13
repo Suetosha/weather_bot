6b435685f53fd0f09acc9bc7f0d976549225cb5f
Suetosha <honey.pancake2017@yandex.ru>
2024-10-13 16:00:15 +0200

    Added session for pythonanywhere hosting

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 from os import getenv
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 from handlers import main_handlers, weather_handlers
 
 load_dotenv()
@@ -9,6 +10,8 @@ load_dotenv()
 TOKEN = getenv("TOKEN")
 
 dp = Dispatcher()
+session = AiohttpSession(proxy='http://proxy.server:3128')
+
 
 # Регистрация роутеров
 dp.include_router(main_handlers.router)
@@ -16,7 +19,7 @@ dp.include_router(weather_handlers.router)
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, session=session)
     await dp.start_polling(bot)
 
 
